be8ab32e5fa3083173de3110f5277bdc94a92486
Jose Camacho <joserracamacho@gmail.com>
2022-03-03 14:00:58 -0500

    Cleanup fonts, stablish new directory structure

--- lib/welcome_css/engine.rb
+++ lib/welcome_css/engine.rb
@@ -1,7 +1,4 @@
 module WelcomeCss
   class Engine < ::Rails::Engine
-    initializer "welcome_css.assets" do
-      Rails.application.config.assets.precompile += %w( welcome.css )
-    end
   end
 end
