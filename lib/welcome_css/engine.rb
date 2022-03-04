b48cc54ee008d336fa6140c62d0d0188de55a726
Jose Camacho <joserracamacho@gmail.com>
2022-03-04 13:51:49 -0500

    Add main asset for precompilation

--- lib/welcome_css/engine.rb
+++ lib/welcome_css/engine.rb
@@ -1,4 +1,7 @@
 module WelcomeCss
   class Engine < ::Rails::Engine
+    initializer "welcome_css.assets.precompile" do |app|
+      app.config.assets.precompile += %w( welcome.css )
+    end
   end
 end
